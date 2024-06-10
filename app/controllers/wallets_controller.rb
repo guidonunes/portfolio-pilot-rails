705c86ed950baedecd192e3fc018a4c4bbcc019a
Guilherme Nunes <guilherme.augd@gmail.com>
2024-06-10 17:41:12 -0300

    implement edit/update action

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -1,2 +1,14 @@
 class WalletsController < ApplicationController
+
+  def edit
+    @wallet = Wallet.find(params[:id])
+  end
+
+  def update
+    if @wallet.update(wallet_params)
+      redirect_to @wallet, notice: 'Wallet has been successfully updated.', status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 end
