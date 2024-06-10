ec91627e976380dbafdf577de91cdc0ecab58afe
Camilly Aoki <camilly.ferreira@icloud.com>
2024-06-10 17:21:35 -0300

    created new

--- app/controllers/wallets_controller.rb
+++ app/controllers/wallets_controller.rb
@@ -1,2 +1,22 @@
 class WalletsController < ApplicationController
+
+  def new
+    @wallet = Wallet.new
+  end
+
+  def create
+    @wallet = Wallet.new(params[:wallet_params])
+    @wallet.user = current_user
+    if @wallet.save
+      redirect_to @wallet, notice: "Wallet was successfully created"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def wallet_params
+    params.require(:wallet).permit(:name, :user_id)
+  end
 end
