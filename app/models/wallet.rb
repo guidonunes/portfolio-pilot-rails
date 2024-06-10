c02e8305aa8f386d3434459fd48e94e2cc2ab59b
Kaio Ghiotti <kaio.sfw@gmail.com>
2024-06-10 15:06:11 -0300

    Add validations and fixed stocks table

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,3 +1,5 @@
 class Wallet < ApplicationRecord
   belongs_to :user
+  has_many :stocks, dependent: :destroy
+  has_many :assets, through: :stocks
 end
--- test/models/stock_test.rb
+++ test/models/stock_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class PortfolioTest < ActiveSupport::TestCase
+class StockTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
