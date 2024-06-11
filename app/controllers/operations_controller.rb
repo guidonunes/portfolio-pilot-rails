dc2e75ba504dcd8c3c7b352b7826b89aa50814a2
Camilly Aoki <camilly.ferreira@icloud.com>
2024-06-11 16:51:56 -0300

    added some features in new from operations

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -1,10 +1,29 @@
 class OperationsController < ApplicationController
   def new
+    @wallet = Wallet.find(params[:wallet_id])
+    @operation = Operation.new
+    @operation.wallet = @wallet
+    authorize @operation
   end
 
   def create
+    @wallet = Wallet.find(params[:wallet_id])
+    @operation = Operation.new(operation_params)
+    @operation.wallet = @wallet
+    authorize @operation
+    if @operation.save
+      redirect_to wallet_path(@wallet)
+    else
+      render :new
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def operation_params
+    params.require(:operation).permit(:holding_id, :quantity, :initial_price)
   end
 end
